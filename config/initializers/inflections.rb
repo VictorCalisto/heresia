aec7bad8e6b0644a6513569279ff195e57985144
victorcalisto <victorcalisto42@email.com>
2024-07-18 00:03:19 -0300

    inflexao

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -11,6 +11,42 @@
 # end
 
 # These inflection rules are supported but not enabled by default:
-# ActiveSupport::Inflector.inflections(:en) do |inflect|
-#   inflect.acronym "RESTful"
-# end
+ActiveSupport::Inflector.inflections(:pt) do |inflect|
+  inflect.acronym "RESTful"
+  
+  # Regras de pluralizacao
+  inflect.plural /$/, 's'               # carro -> carros
+  inflect.plural /r$/i, 'res'           # mulher -> mulheres
+  inflect.plural /l$/i, 'is'            # animal -> animais
+  inflect.plural /m$/i, 'ns'            # homem -> homens
+  inflect.plural /ao$/i, 'oes'          # cao -> caes
+  inflect.plural /el$/i, 'eis'          # papel -> papeis
+  inflect.plural /il$/i, 'is'           # barril -> barris
+  inflect.plural /ul$/i, 'uis'          # azul -> azuis
+  inflect.plural /s$/i, 'ses'           # pais -> paises
+  inflect.plural /z$/i, 'zes'           # cruz -> cruzes
+  inflect.plural /al$/i, 'ais'          # animal -> animais
+  inflect.plural /ol$/i, 'ois'          # sol -> sois
+  inflect.plural /ul$/i, 'uis'          # azul -> azuis
+  inflect.plural /x$/i, 'xes'           # tox -> toxes
+
+  # Regras de singularizacao
+  inflect.singular /s$/i, ''            # carros -> carro
+  inflect.singular /res$/i, 'r'         # mulheres -> mulher
+  inflect.singular /is$/i, 'l'          # animais -> animal
+  inflect.singular /ns$/i, 'm'          # homens -> homem
+  inflect.singular /oes$/i, 'ao'        # caes -> cao
+  inflect.singular /eis$/i, 'el'        # papeis -> papel
+  inflect.singular /is$/i, 'il'         # barris -> barril
+  inflect.singular /uis$/i, 'ul'        # azuis -> azul
+  inflect.singular /ses$/i, 's'         # paises -> pais
+  inflect.singular /zes$/i, 'z'         # cruzes -> cruz
+  inflect.singular /ais$/i, 'al'        # animais -> animal
+  inflect.singular /ois$/i, 'ol'        # sois -> sol
+  inflect.singular /xes$/i, 'x'         # toxes -> tox
+
+  # Regras irregulares
+
+  # Palavras nao contaveis
+  inflect.uncountable %w(torax tenis lapis fenix onibus caris lupus pires climax parentesis triceps biceps duplex triplex)
+end
